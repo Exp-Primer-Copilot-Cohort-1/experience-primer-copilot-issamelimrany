a9ae4f5bb57cf38614e0d6def66fa2e7c8c56a99
ISSAM EL IMRANY <90471805+issamelimrany@users.noreply.github.com>
2024-02-13 10:48:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember(){
+    return{
+        restrict : "E",
+        templateURL : "modules/skills/views/member.html", 
+        controller : "SkillsMemberController", 
+        controllerAS : "vm", 
+        bindToController : true, 
+    }
+}
